01a87225b98f1c30110e955b890af24d8866ab1d
jadiel <jadiel@SPED>
2016-04-30 11:51:22 -0300

    Criando Seed Client

--- database/seeds/ClientTableSeeder.php
+++ database/seeds/ClientTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ClientTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,9 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        Model::unguard();
-        
-        $this->call(ClientTableSeeder::class);
+        factory(\CodeProject\Client::class,10)->create();
     }
 }
